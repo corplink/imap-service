8aef6c26b581b748870bd6eda49d14754660dce6
yoonvelop <yoonvelop@gmail.com>
2019-12-03 14:52:45 +0900

    add objects
    add object class

---
+++ objects/member/Member.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Member
+{
+    private $table_name = "member";
+
+    public $mem_no;
+    public $mem_id;
+    public $mem_password;
+    public $mem_nickname;
+    public $mem_name;
+    public $mem_company;
+    public $mem_dept;
+    public $mem_phone;
+    public $mem_state;
+    public $mem_ins_dt;
+    public $mem_del_dt;
+
+}
